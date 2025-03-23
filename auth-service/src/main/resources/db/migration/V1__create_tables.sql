92eeb6fc02b26791ea6a5f7c54cc7c9dc3eccfbb
Juan Carlos A <abjuancarlos.12@gmail.com>
2025-03-23 16:37:02 -0500

    fix: sql scrit was fixed

--- auth-service/src/main/resources/db/migration/V1__create_tables.sql
+++ auth-service/src/main/resources/db/migration/V1__create_tables.sql
@@ -3,18 +3,18 @@ CREATE TABLE IF NOT EXISTS user_entity(
       username VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS product_entity{
+CREATE TABLE IF NOT EXISTS product_entity(
       id UUID PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       description VARCHAR(255) NOT NULL,
-      price DOUBLE PRESICION,
+      price DOUBLE PRECISION,
       quiantity INTEGER,
       created_at DATE
-}
+    );
 
-CREATE TABLE IF NOT EXISTS historical_product_price{
-      price DOUBLE PRESICION,
+CREATE TABLE IF NOT EXISTS historical_product_price(
+      price DOUBLE PRECISION,
       product_id UUID,
       created_at DATE,
       PRIMARY KEY(price, product_id, created_at)
-}
+);
